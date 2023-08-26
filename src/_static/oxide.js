82fc1a216ae2d2004d982b7005d0b79a3cfffe2e
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2023-08-25 22:15:50 -0600

    Add alternative fullscreen mechanism

--- src/_static/oxide.js
+++ src/_static/oxide.js
@@ -29,14 +29,28 @@ window.addEventListener('DOMContentLoaded', function(){
         .querySelectorAll('img.screenshot')
         .forEach(function(img){
             img.addEventListener('click', function(){
-                if(!document.fullscreenElement){
-                    img
-                    .requestFullscreen({ navigationUI: "show" })
-                    .catch(e => console.error(e));
-                }else{
-                    document
-                        .exitFullscreen();
+                if(document.fullscreenEnabled){
+                    if(!document.fullscreenElement){
+                        img
+                        .requestFullscreen({ navigationUI: "show" })
                         .catch(e => console.error(e));
+                    }else{
+                        document
+                            .exitFullscreen()
+                            .catch(e => console.error(e));
+                    }
+                }else{
+                    let el = document.createElement("img");
+                    el.src = img.src;
+                    el.title = "Click to close image";
+                    el.classList.add("fullscreen");
+                    el.addEventListener('click', () => document.body.removeChild(el));
+                    el.addEventListener('keypress', function(e){
+                        if(e.key == "Escape" || e.key == "F11"){
+                            document.body.removeChild(el);
+                        }
+                    });
+                    document.body.appendChild(el);
                 }
             });
         });
