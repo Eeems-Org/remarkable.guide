5845034bcec39102e5c2cd85c6be3500b02c4fa0
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2023-11-05 13:01:10 -0700

    Get metatags working

--- src/conf.py
+++ src/conf.py
@@ -1,3 +1,4 @@
+"""sphinx configuration"""
 import time
 from datetime import datetime
 
@@ -48,6 +49,17 @@ html_js_files = [
             "async": "async",
         },
     ),
+]
+
+ogp_site_url = html_baseurl
+ogp_description_length = 200
+ogp_site_name = project
+ogp_image = "/_static/images/favicon.svg"
+ogp_use_first_image = True
+ogp_type = "article"
+ogp_enable_meta_description = True
+ogp_custom_meta_tags = [
+    f'<meta property="og:article:modified_time" content="{datetime.utcnow().isoformat()}" />',
 ]
 
 # Do not enable sphinx.ext.autosectionlabel
